a2d7065efee06ee74306f863f96a5c41755267cc
PancrasL <pengzelv@qq.com>
2022-04-30 20:09:35 +0800

    商品分类维护信息请求

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  data () {
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    // 获取商品分类列表
+    getMenus () {
+      this.dataListLoading = true
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log('Sucess: ' + data)
+      })
+    }
+  },
+  // 生命周期-创建完成
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
