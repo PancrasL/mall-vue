68efb0de92bdcf188cd6a337bbb95907f6a2945d
PancrasL <pengzelv@qq.com>
2022-05-01 11:41:15 +0800

    完成删除分类的前端代码

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,15 +1,31 @@
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox
+           node-key="catId" :default-expanded-keys="expandedKey">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level<=2" type="text" size="mini"
+                     @click="() => append(data)">Append</el-button>
+          <el-button v-if="node.childNodes.length===0" type="text" size="mini"
+                     @click="() => remove(node, data)">Delete</el-button>
+        </span>
+      </span>
+  </el-tree>
 </template>
 
 <script>
 export default {
   data () {
+    return {
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
   },
   methods: {
-    handleNodeClick (data) {
-      console.log(data)
-    },
     // 获取商品分类列表
     getMenus () {
       this.dataListLoading = true
@@ -17,13 +33,60 @@ export default {
         url: this.$http.adornUrl('/product/category/list/tree'),
         method: 'get'
       }).then(({data}) => {
-        console.log('Sucess: ' + data)
+        this.menus = data.data
+      })
+    },
+    // 添加树形条目
+    append (data) {
+      console.log('append', data)
+    },
+    // 删除树形条目
+    remove (node, data) {
+      this.$confirm(`此操作将删除[${data.name}]分类, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let ids = [data.catId]
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(() => {
+          this.$message({
+            message: '删除成功',
+            type: 'success'
+          })
+          this.getMenus()
+          // 设置需要默认展开的菜单
+          this.expandedKey = [node.parent.data.catId]
+        })
+      }).catch(() => {
+        this.$message({
+          message: '取消成功'
+        })
       })
     }
   },
   // 生命周期-创建完成
   created () {
     this.getMenus()
+  },
+  mounted () {
+  },
+  beforeCreate () {
+  },
+  beforeMount () {
+  },
+  beforeUpdate () {
+  },
+  updated () {
+  },
+  beforeDestroy () {
+  },
+  destroyed () {
+  },
+  activated () {
   }
 }
 </script>
